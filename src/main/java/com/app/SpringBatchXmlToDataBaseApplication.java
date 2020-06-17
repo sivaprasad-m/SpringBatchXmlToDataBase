25f402b387c740ea8e3fbb1128c010b91431b8c3
MVS PRASAD <venkatasiva043@gmail.com>
2020-06-17 08:30:49 +0530

    first commit

---
+++ src/main/java/com/app/SpringBatchXmlToDataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchXmlToDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchXmlToDataBaseApplication.class, args);
+	}
+
+}
